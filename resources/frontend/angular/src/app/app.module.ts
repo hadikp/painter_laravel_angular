c6b9a786fe40f92bc80da63db5b3d897ec4c81f9
hadikp <hp20001025@gmail.com>
2021-05-13 16:23:15 +0200

    menu start

--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CeruzarajzComponent } from './pages/ceruzarajz/ceruzarajz.component';
+import { PastelrajzComponent } from './pages/pastelrajz/pastelrajz.component';
+import { AkvarellComponent } from './pages/akvarell/akvarell.component';
+import { OlajfestmenyComponent } from './pages/olajfestmeny/olajfestmeny.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    CeruzarajzComponent,
+    PastelrajzComponent,
+    AkvarellComponent,
+    OlajfestmenyComponent
   ],
   imports: [
     BrowserModule,
