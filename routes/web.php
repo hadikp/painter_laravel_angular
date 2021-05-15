6091a51d60df222f3c9879a0c5f9147b7b0c6216
hadikp <hp20001025@gmail.com>
2021-05-15 21:51:49 +0200

    laravel auth

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-/* Route::get('/', function () {
+ Route::get('/', function () {
     return view('welcome');
-}); */
+}); 
 //Route::get('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
-Route::get('/', 'AngularController@index');
+//Route::get('/', 'AngularController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
